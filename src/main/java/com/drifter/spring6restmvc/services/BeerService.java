48693acd3c12ce6612f7ce938938fdc8ae560bff
DonMB77 <purposefuldrifter@gmail.com>
2025-01-02 21:41:11 +0100

    BeerService and Impl refactor

--- src/main/java/com/drifter/spring6restmvc/services/BeerService.java
+++ src/main/java/com/drifter/spring6restmvc/services/BeerService.java
@@ -2,9 +2,12 @@ package com.drifter.spring6restmvc.services;
 
 import com.drifter.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
 }

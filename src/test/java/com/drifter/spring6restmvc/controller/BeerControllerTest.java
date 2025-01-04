49f067256497f8fb8cf7824aa402527b7fdbfb19
DonMB77 <purposefuldrifter@gmail.com>
2025-01-04 15:48:40 +0100

    MockMvc using Matcher in getBeerById Test

--- src/test/java/com/drifter/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/drifter/spring6restmvc/controller/BeerControllerTest.java
@@ -1,6 +1,8 @@
 package com.drifter.spring6restmvc.controller;
 
+import com.drifter.spring6restmvc.model.Beer;
 import com.drifter.spring6restmvc.services.BeerService;
+import com.drifter.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,11 +11,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
@@ -25,11 +30,18 @@ class BeerControllerTest {
     @MockitoBean
     BeerService beerService;
 
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
     @Test
     void getBeerById() throws Exception {
 
+        Beer testBeer = beerServiceImpl.listBeers().getFirst();
+
+        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }

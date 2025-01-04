8a436bf0b79f5f40f934443e9b8913d4050679d4
DonMB77 <purposefuldrifter@gmail.com>
2025-01-04 20:16:17 +0100

    CustomerControllerTest testSaveCustomer()

--- src/test/java/com/drifter/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/com/drifter/spring6restmvc/controller/CustomerControllerTest.java
@@ -3,6 +3,8 @@ package com.drifter.spring6restmvc.controller;
 import com.drifter.spring6restmvc.model.Customer;
 import com.drifter.spring6restmvc.services.CustomerService;
 import com.drifter.spring6restmvc.services.CustomerServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,8 +13,10 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CustomerController.class)
@@ -25,6 +29,30 @@ class CustomerControllerTest {
 
     @MockitoBean
     CustomerService customerService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        CustomerServiceImpl customerService1 = new CustomerServiceImpl();
+    }
+
+    @Test
+    void testSaveCustomer () throws Exception {
+        Customer newCustomer = customerServiceImpl.getAllCustomers().getFirst();
+        newCustomer.setId(null);
+
+        given(customerService.saveCustomer(any(Customer.class))).willReturn(customerServiceImpl.getAllCustomers().get(1));
+
+        mockMvc.perform(post("/api/v1/customer")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newCustomer)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
+
+    }
 
     @Test
     void getAllCustomers() throws Exception{

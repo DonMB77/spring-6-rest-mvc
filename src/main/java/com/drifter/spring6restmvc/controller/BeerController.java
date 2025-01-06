25a559342c9bf7278b2eef1929f1cb3dcca1c978
DonMB77 <purposefuldrifter@gmail.com>
2025-01-06 12:57:30 +0100

    Added NotFoundException class as well as exception handling geBeerById()

--- src/main/java/com/drifter/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/drifter/spring6restmvc/controller/BeerController.java
@@ -65,6 +65,13 @@ public class BeerController {
         return beerService.listBeers();
     }
 
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException() {
+        System.out.println("In exception handler");
+
+        return ResponseEntity.notFound().build();
+    }
+
     @GetMapping(BEER_PATH_ID)
     public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
 

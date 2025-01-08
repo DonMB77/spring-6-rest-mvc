1a00cafd9f263bf61b8a87c20a52d38779cb7e04
DonMB77 <purposefuldrifter@gmail.com>
2025-01-08 21:23:02 +0100

    Implementation updateBeerById test and implementation in BeerServiceJPA

--- src/test/java/com/drifter/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/com/drifter/spring6restmvc/controller/BeerControllerIT.java
@@ -1,6 +1,7 @@
 package com.drifter.spring6restmvc.controller;
 
 import com.drifter.spring6restmvc.entities.Beer;
+import com.drifter.spring6restmvc.mappers.BeerMapper;
 import com.drifter.spring6restmvc.model.BeerDTO;
 import com.drifter.spring6restmvc.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
@@ -24,6 +25,25 @@ class BeerControllerIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Autowired
+    BeerMapper beerMapper;
+
+    @Test
+    void updateExistingBeer() {
+        Beer beer = beerRepository.findAll().getFirst();
+        BeerDTO beerDTO = beerMapper.beerToBeerDto(beer);
+        beerDTO.setId(null);
+        beerDTO.setVersion(null);
+        final String beerName = "UPDATED";
+        beerDTO.setBeerName(beerName);
+
+        ResponseEntity responseEntity = beerController.updateById(beer.getId(), beerDTO);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
+
+        Beer updatedBeer = beerRepository.findById(beer.getId()).get();
+        assertThat(updatedBeer.getBeerName()).isEqualTo(beerName);
+    }
 
     @Transactional
     @Rollback

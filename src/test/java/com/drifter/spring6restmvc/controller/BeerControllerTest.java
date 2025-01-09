d03f98fdc5461fe34dd58fd063eec085d99792b0
DonMB77 <purposefuldrifter@gmail.com>
2025-01-09 18:09:14 +0100

    Added Validation to BeerDTO, BeerController and subsequent test

--- src/test/java/com/drifter/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/drifter/spring6restmvc/controller/BeerControllerTest.java
@@ -58,6 +58,20 @@ class BeerControllerTest {
     }
 
     @Test
+    void testCreateBeerNullBeerName() throws Exception {
+
+        // notice how we are not building a beerName
+        BeerDTO beerDTO = BeerDTO.builder().build();
+        given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(beerServiceImpl.listBeers().get(1));
+
+        mockMvc.perform(post(BeerController.BEER_PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beerDTO)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
     void testPatchBeer() throws Exception {
         BeerDTO beerDTO = beerServiceImpl.listBeers().getFirst();
 

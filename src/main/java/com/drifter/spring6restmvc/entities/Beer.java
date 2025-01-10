a9d122ff12a3abad9efe28bf689542631de0e8fc
DonMB77 <purposefuldrifter@gmail.com>
2025-01-10 16:12:34 +0100

    Beer Entity @Size and Column constraint + test

--- src/main/java/com/drifter/spring6restmvc/entities/Beer.java
+++ src/main/java/com/drifter/spring6restmvc/entities/Beer.java
@@ -4,6 +4,7 @@ import com.drifter.spring6restmvc.model.BeerStyle;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UuidGenerator;
@@ -28,6 +29,8 @@ public class Beer {
     private Integer version;
     @NotNull
     @NotBlank
+    @Size(max = 50)
+    @Column(length = 50)
     private String beerName;
     @NotNull
     private BeerStyle beerStyle;
--- src/test/java/com/drifter/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/drifter/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package com.drifter.spring6restmvc.repositories;
 
 import com.drifter.spring6restmvc.entities.Beer;
 import com.drifter.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @DataJpaTest
@@ -16,6 +18,20 @@ class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeerNameTooLong() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("This is a test beer with a very long name. In this case more than the constraint of 50 characters. Oh wow!")
+                    .beerStyle(BeerStyle.PILSNER)
+                    .upc("23456")
+                    .price(BigDecimal.valueOf(14.99))
+                    .build());
+
+            beerRepository.flush();
+        });
+    }
 
     @Test
     void testSaveBeer() {

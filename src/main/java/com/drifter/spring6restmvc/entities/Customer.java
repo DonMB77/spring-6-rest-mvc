0a447b1657666cbc05ee79a601cb6c8eb859c519
DonMB77 <purposefuldrifter@gmail.com>
2025-01-11 22:19:48 +0100

    flyway dependency + V2 migration

--- src/main/java/com/drifter/spring6restmvc/entities/Customer.java
+++ src/main/java/com/drifter/spring6restmvc/entities/Customer.java
@@ -26,6 +26,8 @@ public class Customer {
     @Version
     private Integer version;
     private String costumerName;
+    @Column(length = 255)
+    private String email;
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
 }

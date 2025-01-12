ea671f039cc2ce38651cd7fe3877bf8b0c335ecf
DonMB77 <purposefuldrifter@gmail.com>
2025-01-12 23:05:20 +0100

    opencsv depend. + BeerCsvService + Impl + sub. tests

---
+++ src/main/java/com/drifter/spring6restmvc/services/BeerCsvService.java
@@ -0,0 +1,10 @@
+package com.drifter.spring6restmvc.services;
+
+import com.drifter.spring6restmvc.model.BeerCSVRecord;
+
+import java.io.File;
+import java.util.List;
+
+public interface BeerCsvService {
+    List<BeerCSVRecord> convertCSV(File csvFile);
+}

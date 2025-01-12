e650b27d29cd28b93ff8c925b157f3aa30004fb7
DonMB77 <purposefuldrifter@gmail.com>
2025-01-13 00:01:11 +0100

    BootstrapData loadCsvData() + Test

--- src/main/java/com/drifter/spring6restmvc/bootstrap/BootstrapData.java
+++ src/main/java/com/drifter/spring6restmvc/bootstrap/BootstrapData.java
@@ -2,23 +2,27 @@ package com.drifter.spring6restmvc.bootstrap;
 
 import com.drifter.spring6restmvc.entities.Beer;
 import com.drifter.spring6restmvc.entities.Customer;
+import com.drifter.spring6restmvc.model.BeerCSVRecord;
 import com.drifter.spring6restmvc.model.BeerDTO;
 import com.drifter.spring6restmvc.model.BeerStyle;
 import com.drifter.spring6restmvc.model.CustomerDTO;
 import com.drifter.spring6restmvc.repositories.BeerRepository;
 import com.drifter.spring6restmvc.repositories.CustomerRepository;
+import com.drifter.spring6restmvc.services.BeerCsvService;
 import com.drifter.spring6restmvc.services.BeerServiceJPA;
 import com.drifter.spring6restmvc.services.CustomerServiceJPA;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Component
 @RequiredArgsConstructor
@@ -26,12 +30,46 @@ public class BootstrapData implements CommandLineRunner {
 
     private final BeerRepository beerRepository;
     private final CustomerRepository customerRepository;
-    private Map<UUID, BeerDTO> beerMap;
+    private final BeerCsvService beerCsvService;
 
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
         loadBeerData();
+        loadCsvData();
         loadCustomerData();
+    }
+
+    private void loadCsvData() throws FileNotFoundException {
+        if (beerRepository.count() < 10){
+            File file = ResourceUtils.getFile("classpath:csvdata/beers.csv");
+
+            List<BeerCSVRecord> recs = beerCsvService.convertCSV(file);
+
+            recs.forEach(beerCSVRecord -> {
+                BeerStyle beerStyle = switch (beerCSVRecord.getStyle()) {
+                    case "American Pale Lager" -> BeerStyle.LAGER;
+                    case "American Pale Ale (APA)", "American Black Ale", "Belgian Dark Ale", "American Blonde Ale" ->
+                            BeerStyle.ALE;
+                    case "American IPA", "American Double / Imperial IPA", "Belgian IPA" -> BeerStyle.IPA;
+                    case "American Porter" -> BeerStyle.PORTER;
+                    case "Oatmeal Stout", "American Stout" -> BeerStyle.STOUT;
+                    case "Saison / Farmhouse Ale" -> BeerStyle.SAISON;
+                    case "Fruit / Vegetable Beer", "Winter Warmer", "Berliner Weissbier" -> BeerStyle.WHEAT;
+                    case "English Pale Ale" -> BeerStyle.PALE_ALE;
+                    default -> BeerStyle.PILSNER;
+                };
+
+                beerRepository.save(Beer.builder()
+                        .beerName(StringUtils.abbreviate(beerCSVRecord.getBeer(), 50))
+                        .beerStyle(beerStyle)
+                        .price(BigDecimal.TEN)
+                        .upc(beerCSVRecord.getRow().toString())
+                        .quantityOnHand(beerCSVRecord.getCount())
+                        .build());
+            });
+
+        }
     }
 
     private void loadBeerData() {
@@ -70,9 +108,10 @@ public class BootstrapData implements CommandLineRunner {
             beerRepository.save(beer2);
             beerRepository.save(beer3);
 
-            beerRepository.saveAll(Arrays.asList(beer1,beer2,beer3));
+            beerRepository.saveAll(Arrays.asList(beer1, beer2, beer3));
         }
     }
+
     private void loadCustomerData() {
         if (customerRepository.count() == 0) {
             Customer customer1 = Customer.builder()
--- src/main/java/com/drifter/spring6restmvc/services/BeerCsvServiceImpl.java
+++ src/main/java/com/drifter/spring6restmvc/services/BeerCsvServiceImpl.java
@@ -2,12 +2,15 @@ package com.drifter.spring6restmvc.services;
 
 import com.drifter.spring6restmvc.model.BeerCSVRecord;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+@Service
 public class BeerCsvServiceImpl implements BeerCsvService {
     @Override
     public List<BeerCSVRecord> convertCSV(File csvFile) {

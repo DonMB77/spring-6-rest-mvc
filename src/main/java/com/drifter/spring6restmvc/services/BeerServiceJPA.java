ea09fd2ba9ede73621151fa20d9983ed8f05866c
DonMB77 <purposefuldrifter@gmail.com>
2025-01-13 23:18:47 +0100

    BeerServiceJPA.listBeers() first impl + sub tests

--- src/main/java/com/drifter/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/com/drifter/spring6restmvc/services/BeerServiceJPA.java
@@ -1,5 +1,6 @@
 package com.drifter.spring6restmvc.services;
 
+import com.drifter.spring6restmvc.entities.Beer;
 import com.drifter.spring6restmvc.mappers.BeerMapper;
 import com.drifter.spring6restmvc.model.BeerDTO;
 import com.drifter.spring6restmvc.repositories.BeerRepository;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -23,10 +25,23 @@ public class BeerServiceJPA implements BeerService {
 
     @Override
     public List<BeerDTO> listBeers(String beerName) {
-        return beerRepository.findAll()
-                .stream()
+
+        List<Beer> beerList;
+
+        if (StringUtils.hasText(beerName)) {
+            beerList = listBeersByName(beerName);
+
+        } else {
+            beerList = beerRepository.findAll();
+        }
+
+        return beerList.stream()
                 .map(beerMapper::beerToBeerDto)
                 .collect(Collectors.toList());
+    }
+
+    public List<Beer> listBeersByName(String beerName) {
+        return beerRepository.findAllByBeerNameIsLikeIgnoreCase("%" + beerName + "%");
     }
 
     @Override

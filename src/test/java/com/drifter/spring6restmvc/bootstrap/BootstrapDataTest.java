1a4957ba1d6775de8a6dd8ddff4b5b5c4b15031b
DonMB77 <purposefuldrifter@gmail.com>
2025-01-13 18:11:19 +0100

    Test updates + Beer Entity
    Test updates + Beer Entity @CreationTimestamp/@UpdateTimestamp

--- src/test/java/com/drifter/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/com/drifter/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -6,15 +6,18 @@ import com.drifter.spring6restmvc.services.BeerCsvService;
 import com.drifter.spring6restmvc.services.BeerCsvServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.testcontainers.shaded.org.checkerframework.checker.units.qual.A;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootstrapDataTest {
 
     @Autowired
@@ -37,7 +40,7 @@ class BootstrapDataTest {
     void testRun() throws Exception {
         bootstrapData.run();
 
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isGreaterThan(0);
         assertThat(customerRepository.count()).isEqualTo(3);
     }
 }

02721a4c0dbeb65c1f7f12bcc50d92860f6a5509
DonMB77 <purposefuldrifter@gmail.com>
2025-01-15 12:57:09 +0100

    BeerOrder Helper Method for Bidirectional Relation

--- src/main/java/com/drifter/spring6restmvc/entities/Customer.java
+++ src/main/java/com/drifter/spring6restmvc/entities/Customer.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.*;
 import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
@@ -33,6 +34,7 @@ public class Customer {
     @UpdateTimestamp
     private LocalDateTime lastModifiedDate;
 
+    @Builder.Default
     @OneToMany(mappedBy = "customer")
-    private Set<BeerOrder> beerOrders;
+    private Set<BeerOrder> beerOrders = new HashSet<>();
 }

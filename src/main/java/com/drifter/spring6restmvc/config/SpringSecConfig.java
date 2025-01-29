098ea9345c1dc1009be80ee30d3534a1357e72e1
DonMB77 <purposefuldrifter@gmail.com>
2025-01-29 19:58:45 +0100

    Springdoc configuration

--- src/main/java/com/drifter/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/com/drifter/spring6restmvc/config/SpringSecConfig.java
@@ -12,7 +12,9 @@ public class SpringSecConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> {
-            authorize.anyRequest().authenticated();
+                    authorize
+                            .requestMatchers("/v3/api-docs**", "swagger-ui/**", "swagger-ui.html").permitAll()
+                            .anyRequest().authenticated();
                 })
                 .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
                     httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());

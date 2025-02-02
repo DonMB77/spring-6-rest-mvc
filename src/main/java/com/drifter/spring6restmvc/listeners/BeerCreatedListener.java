83ab735d4762ee9b13efb227150f67d08cbdf606
DonMB77 <purposefuldrifter@gmail.com>
2025-02-02 21:53:19 +0100

    TaskConfig + EventListener Async

--- src/main/java/com/drifter/spring6restmvc/listeners/BeerCreatedListener.java
+++ src/main/java/com/drifter/spring6restmvc/listeners/BeerCreatedListener.java
@@ -2,14 +2,19 @@ package com.drifter.spring6restmvc.listeners;
 
 import com.drifter.spring6restmvc.events.BeerCreatedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BeerCreatedListener {
 
+    @Async
     @EventListener
     public void listen(BeerCreatedEvent beerCreatedEvent) {
         System.out.println("BeerCreatedListener - I heard a beer was created!");
         System.out.println(beerCreatedEvent.getBeer().getId());
+
+        System.out.println("Current Thread Name: " + Thread.currentThread().getName());
+        System.out.println("Current Thread ID: " + Thread.currentThread().threadId());
     }
 }

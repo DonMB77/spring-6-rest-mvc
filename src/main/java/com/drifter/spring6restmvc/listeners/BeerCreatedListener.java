681f24e27758380fb00239a8240db848f14a5ed8
DonMB77 <purposefuldrifter@gmail.com>
2025-02-02 22:32:27 +0100

    BeerAuditRepository

--- src/main/java/com/drifter/spring6restmvc/listeners/BeerCreatedListener.java
+++ src/main/java/com/drifter/spring6restmvc/listeners/BeerCreatedListener.java
@@ -1,20 +1,35 @@
 package com.drifter.spring6restmvc.listeners;
 
 import com.drifter.spring6restmvc.events.BeerCreatedEvent;
+import com.drifter.spring6restmvc.mappers.BeerMapper;
+import com.drifter.spring6restmvc.repositories.BeerAuditRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class BeerCreatedListener {
+
+    private final BeerMapper beerMapper;
+    private final BeerAuditRepository beerAuditRepository;
 
     @Async
     @EventListener
     public void listen(BeerCreatedEvent beerCreatedEvent) {
-        System.out.println("BeerCreatedListener - I heard a beer was created!");
-        System.out.println(beerCreatedEvent.getBeer().getId());
 
-        System.out.println("Current Thread Name: " + Thread.currentThread().getName());
-        System.out.println("Current Thread ID: " + Thread.currentThread().threadId());
+        val beerAudit = beerMapper.beerToBeerAudit(beerCreatedEvent.getBeer());
+        beerAudit.setAuditEventType("BEER_CREATED");
+
+        if (beerCreatedEvent.getAuthentication() != null && beerCreatedEvent.getAuthentication().getName() != null) {
+            beerAudit.setPrincipalName(beerCreatedEvent.getAuthentication().getName());
+        }
+
+        val savedBeerAudit = beerAuditRepository.save(beerAudit);
+        log.debug("Beer Audit Saved: {}", savedBeerAudit.getId());
     }
 }

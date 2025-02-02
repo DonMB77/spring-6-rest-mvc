b85b89e12bbaf6efcd5fe93801541b8e8ea377f4
DonMB77 <purposefuldrifter@gmail.com>
2025-02-02 22:15:33 +0100

    BeerMapper.beerToBeerAudit()

--- src/main/java/com/drifter/spring6restmvc/mappers/BeerMapper.java
+++ src/main/java/com/drifter/spring6restmvc/mappers/BeerMapper.java
@@ -1,13 +1,23 @@
 package com.drifter.spring6restmvc.mappers;
 
 import com.drifter.spring6restmvc.entities.Beer;
+import com.drifter.spring6restmvc.entities.BeerAudit;
 import com.drifter.spring6restmvc.model.BeerDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface BeerMapper {
 
+    @Mapping(target = "categories", ignore = true)
+    @Mapping(target = "beerOrderLines", ignore = true)
     Beer beerDtoToBeer(BeerDTO dto);
 
     BeerDTO beerToBeerDto(Beer beer);
+
+    @Mapping(target = "createdDateAudit", ignore = true)
+    @Mapping(target = "auditId", ignore = true)
+    @Mapping(target = "auditEventType", ignore = true)
+    @Mapping(target = "principalName", ignore = true)
+    BeerAudit beerToBeerAudit(Beer beer);
 }
